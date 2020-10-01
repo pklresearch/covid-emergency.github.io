2602aa5df6266edc2c27bcaaa21f04bdb87d4fb1
unknown <pklresearch>
2020-10-01 11:09:50 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--
+Template Name: Oleald
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+-->
+<html lang="">
+<!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
+<head>
+<title>Covid Emergency</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay light" style="background-image:url('images/demo/backgrounds/4.jpg');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left">
+       <!-- ################################################################################################ --> 
+      </div>
+      <div class="fl_right"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">Covid Emergency</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a class="drop" href="#">Pages</a>
+            <ul>
+              <li><a href="pages/gallery.html">Detail</a></li>
+              <li><a href="login">login</a></li>
+              <li><a href="pages/sidebar-left.html">about</a></li>
+            </ul>
+          </li>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="group excerpt">
+      <article class="one_third first"><a class="imgover btmspace-30" href="#"><img src="file:///C:/Users/fikri/Pictures/rsud-massen-rempulu-2.jpg" alt=""></a>
+        <h6 class="heading">RSUD Massenrempulu Enkerang </h6>
+        <p>Rumah Sakit Umum Massenrempulu menerima Bantuan alat Pemeriksa Kesehatan berupa alat alat rontgen 4 dimensi yang diperoleh dari bantuan pemerintah pusat &hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="file:///C:/Users/fikri/Pictures/IMG_20190402_132323-e1554182803729.jpg" alt=""></a>
+        <h6 class="heading">RSUD H.ANDI SULTHAN DAENG RADJA </h6>
+        <p>Rumah Sakit Umum Bulukumba didirikan pertama kali pada tahun 1969 yang dulunya terletak di Jalan dr. Soetomo (sekarang menjadi Kantor Dinas Kesehatan) di samping lapangan Pemuda. Lalu dibangun gedung rumah sakit umum yang baru yang berada di lokasi Jalan&hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="file:///C:/Users/fikri/Pictures/2017-01-02.jpg" alt=""></a>
+        <h6 class="heading">RSU Thalia irham</h6>
+        <p>Rumah Sakit Umum Thalia Irham mengambil lokasi di tepi Jalan Raya Poros Sungguminasa Limbung, km 15 Desa Panciro, Kecamatan Bajeng Kabupaten Gowa.Lokasi ini sekitar 15 km dari kota Makassar, 4 km dari pusat kota Gowa     (Sungguminasa).&hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+  </section>
+</div>
+<div class="wrapper row3">
+  <figure class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <figcaption class="sectiontitle">
+      <h6 class="heading">Gambar Kota yang Terkena Covid-19</h6>
+    </figcaption>
+    <ul class="nospace group center overview">
+      <li class="one_third"><a class="imgover" ><img src="images/demo/320x418.jpg" alt=""></a></li>
+      <li class="one_third"><a class="imgover" ><img src="images/demo/bg.jpg" alt=""></a></li>
+      <li class="one_third"><a class="imgover" ><img src="images/demo/5.jpg" alt=""></a></li>
+    </ul>
+    <footer class="center"><a class="btn" href="#">View Mors</a></footer>
+    <!-- ################################################################################################ -->
+  </figure>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2 bgded overlay" style="background-image:url('images/demo/backgrounds/3.jpg');">
+  <section class="hoc container clear testimonials"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">2 dokter yang menemukan covid pertamakali</h6>
+      <p></p>
+    </div>
+    <ul class="nospace group btmspace-80">
+      <li class="one_half first">
+        <blockquote></blockquote>
+        <figure class="clear"><img src="file:///C:/Users/fikri/Pictures/picture.jpeg" alt="">
+          <figcaption>
+            <h6 class="heading">Dr.Zhang Jixian</h6>
+            <em>CDC</em></figcaption>
+        </figure>
+      </li>
+      <li class="one_half">
+        <blockquote></blockquote>
+        <figure class="clear"><img src="file:///C:/Users/fikri/Pictures/image.jpg" alt="">
+          <figcaption>
+            <h6 class="heading">Dr.June Almeida</h6>
+            <em>Sales Director</em></figcaption>
+        </figure>
+      </li>
+    </ul>
+    <footer class="center"><a class="btn" href="#">View More &raquo;</a></footer>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2 bgded overlay" style="background-image:url('images/demo/backgrounds/2.jpg');">
+  <section class="hoc cta clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Silahkan Cari Rumah Sakit Terdekat</h6>
+    </div>
+    <form method="post" action="#">
+      <fieldset>
+        <legend>Newsletter:</legend>
+        <input class="btmspace-15" type="text" value="" placeholder="Enter Your Email Here&hellip;">
+        <button type="submit" value="submit">Submit</button>
+      </fieldset>
+    </form>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_quarter">
+      <h6 class="heading">Placerat phasellus</h6>
+      <ul class="nospace btmspace-30 linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          Street Name &amp; Number, Town, Postcode/Zip
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> 085759455000</li>
+        <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+      </ul>
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="https://www.facebook.com/?stype=lo&jlou=AfewFvz1xIaOH6T7Zcr2OpdFG03X8PPuWQcodV-PK0e9P9zno708uFyGRa2M1ShSiXRdvxGUwf3TBf3LgVGjS5BvsRSBoSDzvOBHnRvREiPMBA&smuh=26790&lh=Ac-geQJBfRSTUfJb"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-twitter" href="https://twitter.com/login?lang=id"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2020 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">wellcome <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">To my web</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
